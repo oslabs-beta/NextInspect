c9e09801d02ad400ae5f60871689233d04ac87f5
Lucia Li <luciali149@gmail.com>
2023-06-08 18:05:56 -0400

    Co-authored-by: Michael Bildstein <mbildstein@users.noreply.github.com>

--- chrome-extension/src/pages/app/app.tsx
+++ chrome-extension/src/pages/app/app.tsx
@@ -3,9 +3,14 @@ import NetworkTable from './components/NetworkTable.tsx'
 import {mockData} from './mockData.tsx'
 
 function App() {
-
+  const eventSource = new EventSource('http://localhost:3002/stream/sse');
+  eventSource.onmessage = (e) => {
+    console.log(e.data)
+  }
+  
   return (
     <>
+    
       <h1>NextInspect DevTool</h1>
       <NetworkTable data={mockData}/>
       

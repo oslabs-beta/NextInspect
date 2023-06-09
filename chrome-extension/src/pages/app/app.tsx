9a0a99d653749918263fe70b502cd489f9b74c9b
Yourui <yourui.ruan@gmail.com>
2023-06-09 15:28:16 -0400

    Replaced mock data with OtelData

--- chrome-extension/src/pages/app/app.tsx
+++ chrome-extension/src/pages/app/app.tsx
@@ -1,23 +1,17 @@
 import { useState } from 'react';
 import './App.css'
 import NetworkTable from './components/NetworkTable.tsx'
-import { OtelData , IMockData} from '../../types/types.ts';
+import { OtelData } from '../../types/types.ts';
 
 function App() {
-  const sseStream = new EventSource('http://localhost:3002/stream/sse');
-  const ssrData: OtelData[] = [];
-  sseStream.onmessage = (e) => {
-    ssrData.push(e.data)
-  }
     // would like to refactor so that requests that have already been rendered, do not need to be rendered again
-  const [networkRequests, setNetworkRequests] = useState<IMockData[]>([]);
+  const [networkRequests, setNetworkRequests] = useState<OtelData[]>([]);
 
 
-  const eventSource = new EventSource('http://localhost:3002/stream/sse');
-  eventSource.onmessage = (e) => {
-    const data:IMockData = e.data;
+  const sseStream = new EventSource('http://localhost:3002/stream/sse');
+  sseStream.onmessage = (e) => {
+    const data:OtelData = e.data;
     setNetworkRequests([...networkRequests, data])
-    console.log(e.data)
   }
 
   
@@ -28,7 +22,7 @@ function App() {
     <>
     
       <h1>NextInspect DevTool</h1>
-      <NetworkTable data={ssrData}/>
+      <NetworkTable data={networkRequests}/>
       
     </>
   )

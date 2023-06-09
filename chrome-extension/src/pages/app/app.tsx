f9864f4f62aa6585e7a399b4e1e87cde2fafd9bb
Lucia Li <luciali149@gmail.com>
2023-06-08 23:07:24 -0400

    conditionally rendered spans that share traceIds and added dropdown arrow

--- chrome-extension/src/pages/app/app.tsx
+++ chrome-extension/src/pages/app/app.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './App.css'
 import NetworkTable from './components/NetworkTable.tsx'
-import {mockData} from './mockData.tsx'
+import {mockData, IMockData} from './mockData.tsx'
 
 function App() {
+  // would like to refactor so that requests that have already been rendered, do not need to be rendered again
+  const [networkRequests, setNetworkRequests] = useState<IMockData[]>([]);
+
+
   const eventSource = new EventSource('http://localhost:3002/stream/sse');
   eventSource.onmessage = (e) => {
+    const data:IMockData = e.data;
+    setNetworkRequests([...networkRequests, data])
     console.log(e.data)
   }
+
   
+
+
+
   return (
     <>
     
---
+++ chrome-extension/src/pages/app/components/TableRow.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import { IMockData } from '../mockData';
+
+
+interface TableRowProps {
+  data: IMockData[];
+}
+
+
+const TableRow = ({data} : TableRowProps) => {
+
+  const [rowClick, setRowClick] = useState(false);
+  const multipleSpans = data.length > 1; // check if there are multipleSpans that share a traceId
+
+  function calcTime(sTime: number, eTime: number): number{
+    return eTime - sTime;
+  }
+
+  // console.log({data});
+  return (
+      <>
+      <tr onClick={() => setRowClick(!rowClick)}>
+          <td>{data[0].traceId}</td>
+          <td>{data[0].spanId}</td>
+          <td>{data[0].applicationType}</td>
+          <td>{data[0].originatingService}</td>
+          <td>{data[0].method}</td>
+          <td>{data[0].status}</td>
+          <td>{data[0].protocol}</td>
+          <td>{calcTime(data[0].startTime, data[0].endTime)} ms</td>
+          <td>{data[0].size}</td>
+          <td>{data[0].type}</td>
+          <td>{data[0].urlEndpoint}</td>
+        </tr> 
+        {multipleSpans && rowClick? (  
+          data.slice(1).map((val, key) => {
+            return (
+              <tr className="span" key={key}>
+                <td></td>
+                <td>{val.spanId}</td>
+                <td>{val.applicationType}</td>
+                <td>{val.originatingService}</td>
+                <td>{val.method}</td>
+                <td>{val.status}</td>
+                <td>{val.protocol}</td>
+                <td>{calcTime(val.startTime, val.endTime)} ms</td>
+                <td>{val.size}</td>
+                <td>{val.type}</td>
+                <td>{val.urlEndpoint}</td>
+              </tr>
+            )
+          })
+        ): null
+        }       
+      </>
+        
+  )
+}
+
+export default TableRow;

58cc5f050965d20f4ad401ba6c961a6abbf7dffe
William Nguyen <105384182+wnguye03@users.noreply.github.com>
2023-06-28 20:32:22 -0400

    feature-clientSideNetworkEvents

--- chrome-extension/src/pages/app/app.tsx
+++ chrome-extension/src/pages/app/app.tsx
@@ -9,10 +9,16 @@ function App() {
   const [networkRequests, setNetworkRequests] = useState<OtelData[]>([]);
 
   useEffect(() => {
+    chrome.runtime.onMessage.addListener((message) => {
+      console.log('received chrome message', message)
+    })
+  }, []);
+  
+  useEffect(() => {
     const sseStream = new EventSource('http://localhost:3002/stream/sse');
     sseStream.addEventListener('message', (e) => {
       try {
-        console.log(e.data);
+        // console.log(e.data);
         setNetworkRequests(prevNetworkRequests => [...prevNetworkRequests, JSON.parse(e.data)]);
       } catch (err) {
         console.log('failed', err);
@@ -22,7 +28,7 @@ function App() {
 
 
   useEffect(() => {
-    console.log(networkRequests);
+    // console.log(networkRequests);
   }, [networkRequests]);
  
   const aggregatedData = checkTraceId(networkRequests);

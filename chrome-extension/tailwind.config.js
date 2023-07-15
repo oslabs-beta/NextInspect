c6ba4cfe2039b3a868694d681f3081013e014b78
mbildstein <michael.bildstein@gmail.com>
2023-07-15 10:43:03 -0700

    config changes ready for pr

--- chrome-extension/tailwind.config.js
+++ chrome-extension/tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/panel/index.html',
     './src/pages/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'Avenir', 'Helvetica', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       colors: {
         background: '#292a2d',
         tableRow: '#202124',
-      },
-      fontFamily: {
-        sans: ['Inter', 'Avenir', 'Helvetica', ...defaultTheme.fontFamily.sans],
       },
     },
   },

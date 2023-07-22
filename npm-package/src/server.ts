f21d3fe64f5a96dd16692aeadadf3f688b4b24c9
davidan1989 <111653859+davidan1989@users.noreply.github.com>
2023-07-22 13:07:03 -0500

    testing 7/22

--- npm-package/src/server.ts
+++ npm-package/src/server.ts
@@ -1,14 +1,50 @@
-import express, { Express, NextFunction, Request, Response, ErrorRequestHandler } from 'express';
-import cors from 'cors';
+import express, {
+  Express,
+  NextFunction,
+  Request,
+  Response,
+  ErrorRequestHandler,
+} from 'express'
+import cors from 'cors'
+import { DefaultError } from './types/types'
+import streamRouter from './routers/streamRouter'
 
+const PORT: number = 3002
+const app: Express = express()
 
-const PORT: number = 3002;
-const app: Express = express();
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// connect stream router
+app.use('/stream', streamRouter)
+
+// express general error handler
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const defaultError: DefaultError = {
+      log: 'error at unknown middleware',
+      code: 500,
+      message: 'check error',
+    }
+
+    const newErr = Object.assign({}, defaultError, err)
+    console.log('error:', newErr)
+    return res.status(newErr.code).json(newErr.message)
+  },
+)
 
 // connect to express port
-app.listen(PORT, (): void => {
+app
+  .listen(PORT, (): void => {
     console.log('NextInspect express npm package running on on port:' + PORT)
-}).on("error", function(err) {
+  })
+  .on('error', function (err) {
     //the listener is set up to listen for error events (ie. port already in use or inaccessible), callback function will execute
-    console.log('error: ', err);
-});
+    console.log('error: ', err)
+  })
+export default app
--- npm-package/tests/server.test.js
+++ npm-package/tests/server.test.js
@@ -1,42 +1,41 @@
 const request = require('supertest')
-import app from '../src/app'
-import express, { Express, NextFunction, Request, Response, ErrorRequestHandler } from 'express';
+import app from '../src/server'
+import express, {
+  Express,
+  NextFunction,
+  Request,
+  Response,
+  ErrorRequestHandler,
+} from 'express'
 const server = 'http://localhost:3002'
-const EventSource = require('eventsource');
-import {EventEmitter} from 'events'; 
+const EventSource = require('eventsource')
+import { EventEmitter } from 'events'
 import { streamController } from '../src/controllers/streamController'
 
-//  describe('Express Routes', () => {
-//      describe('/', () => {
-//          it('GET', () => {
-//              return request(server)
-//                  .get('/')
-//                  .expect(404)
-//     })
-//   })
-// })
-
+describe('Express Routes', () => {
+  describe('/', () => {
+    it('GET', () => {
+      return request(server).get('/').expect(404)
+    })
+  })
+})
 
 describe('/stream', () => {
-//Mocking endpoint elements
+  //Mocking endpoint elements
 
-it('should call middleware function', () => {
+  it('should call middleware function', () => {
+    streamController.sendEvent = jest.fn()
 
-const mockSend = jest.fn()
-streamController.sendEvent = mockSend; 
-
-app.get('/stream/sse')
-expect(streamController.sendEvent).toBeCalled()
-
+    app.get('/stream/sse')
+    expect(streamController.sendEvent).toBeCalled()
+  })
 })
-})
-
 
 //Attempt at utilizing EventEmitter and EventSource
 //   let serverEvent, appEvent
 
 // beforeEach(function (done) {
-//   const emitter = new EventEmitter(); 
+//   const emitter = new EventEmitter();
 //   appEvent = express()
 //   serverEvent = appEvent.listen(3000, done)
 // })
@@ -45,77 +44,73 @@ expect(streamController.sendEvent).toBeCalled()
 //   serverEvent.close(done)
 // })
 
-// it ('should call the middleware function', (done) => { 
+// it ('should call the middleware function', (done) => {
 // const eventSource = new EventSource('http://localhost:3000/test')
 
 // emitter.emit('test message', 1500)
 
-// eventSource.addEventListener = ('test message', (data) => { 
+// eventSource.addEventListener = ('test message', (data) => {
 // console.log(data)
 // eventSource.close()
 // done()
 // })
 // })
 
-
-
 // //Request mock to test post route
 const requestMock = {
-    resourceSpans: [
-      {
-        scopeSpans: [
-          {
-            spans: [
-              {
-                traceId: 1,
-                attributes: [
-                  { value: 1 },
-                  { value: 2 },
-                  { value: 3 },
-                  { key: 'http.status_code', value: { intValue: 505 } },
-                  { key: 'http.flavor', value: { stringValue: 'flavor' } },
-                  { key: 'http.target', value: { stringValue: 'target' } },
-                  { key: 'http.method', value: { stringValue: 'http method' } },
-                  {
-                    key: 'http.request_content_length_uncompressed',
-                    value: { intValue: 155 },
-                  },
-                ],
-                spanId: 5,
-                name: 'method name',
-                startTimeUnixNano: 15,
-                endTimeUnixNano: 24,
-              },
-            ],
-          },
-        ],
-        resource: {
-          attributes: [
-            { value: { stringValue: 'nextJs' } },
-            { value: 1 },
-            { value: 2 },
-            { value: 3 },
-            { value: { stringValue: 'internet' } },
-            { value: 5 },
-            { value: 6 },
-            { value: 7 },
-            { value: 8 },
-            { value: 9 },
-            { value: 10 },
-            { value: 11 },
-            { value: 12 },
+  resourceSpans: [
+    {
+      scopeSpans: [
+        {
+          spans: [
+            {
+              traceId: 1,
+              attributes: [
+                { value: 1 },
+                { value: 2 },
+                { value: 3 },
+                { key: 'http.status_code', value: { intValue: 505 } },
+                { key: 'http.flavor', value: { stringValue: 'flavor' } },
+                { key: 'http.target', value: { stringValue: 'target' } },
+                { key: 'http.method', value: { stringValue: 'http method' } },
+                {
+                  key: 'http.request_content_length_uncompressed',
+                  value: { intValue: 155 },
+                },
+              ],
+              spanId: 5,
+              name: 'method name',
+              startTimeUnixNano: 15,
+              endTimeUnixNano: 24,
+            },
           ],
         },
+      ],
+      resource: {
+        attributes: [
+          { value: { stringValue: 'nextJs' } },
+          { value: 1 },
+          { value: 2 },
+          { value: 3 },
+          { value: { stringValue: 'internet' } },
+          { value: 5 },
+          { value: 6 },
+          { value: 7 },
+          { value: 8 },
+          { value: 9 },
+          { value: 10 },
+          { value: 11 },
+          { value: 12 },
+        ],
       },
-    ],
-  }
+    },
+  ],
+}
 
 // //Testing for POST request to /otel route
 describe('/otel', () => {
   it('POST', async () => {
-    const response = await request(app)
-      .post('/stream/otel')
-      .send(requestMock)
+    const response = await request(app).post('/stream/otel').send(requestMock)
     expect(response.body).toEqual('completed')
     expect(response.status).toBe(200)
   })

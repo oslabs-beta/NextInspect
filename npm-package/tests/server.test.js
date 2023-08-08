9c0e00783ed17006837e95e84258923fb44f6303
davidan1989 <111653859+davidan1989@users.noreply.github.com>
2023-08-07 19:36:47 -0500

    test: 💍 Updating Testing
    Updated server and controller tests

--- npm-package/tests/server.test.js
+++ npm-package/tests/server.test.js
@@ -1,16 +1,16 @@
 const request = require('supertest')
-import app from '../src/server'
 import express, {
   Express,
   NextFunction,
   Request,
   Response,
   ErrorRequestHandler,
+  response,
 } from 'express'
 const server = 'http://localhost:3002'
 const EventSource = require('eventsource')
 import { EventEmitter } from 'events'
-import { streamController } from '../src/controllers/streamController'
+import { getHeapStatistics } from 'v8'
 
 describe('Express Routes', () => {
   describe('/', () => {
@@ -18,100 +18,66 @@ describe('Express Routes', () => {
       return request(server).get('/').expect(404)
     })
   })
-})
 
-describe('/stream', () => {
-  //Mocking endpoint elements
-
-  it('should call middleware function', () => {
-    streamController.sendEvent = jest.fn()
-
-    app.get('/stream/sse')
-    expect(streamController.sendEvent).toBeCalled()
-  })
-})
-
-//Attempt at utilizing EventEmitter and EventSource
-//   let serverEvent, appEvent
-
-// beforeEach(function (done) {
-//   const emitter = new EventEmitter();
-//   appEvent = express()
-//   serverEvent = appEvent.listen(3000, done)
-// })
-
-// afterEach(function(done) {
-//   serverEvent.close(done)
-// })
-
-// it ('should call the middleware function', (done) => {
-// const eventSource = new EventSource('http://localhost:3000/test')
-
-// emitter.emit('test message', 1500)
-
-// eventSource.addEventListener = ('test message', (data) => {
-// console.log(data)
-// eventSource.close()
-// done()
-// })
-// })
-
-// //Request mock to test post route
-const requestMock = {
-  resourceSpans: [
-    {
-      scopeSpans: [
-        {
-          spans: [
-            {
-              traceId: 1,
+  describe('/otel', () => {
+    it('POST requests return a response with status 200 and completed in body', async () => {
+      const requestMock = {
+        resourceSpans: [
+          {
+            scopeSpans: [
+              {
+                spans: [
+                  {
+                    traceId: 1,
+                    attributes: [
+                      { value: 1 },
+                      { value: 2 },
+                      { value: 3 },
+                      { key: 'http.status_code', value: { intValue: 505 } },
+                      { key: 'http.flavor', value: { stringValue: 'flavor' } },
+                      { key: 'http.target', value: { stringValue: 'target' } },
+                      {
+                        key: 'http.method',
+                        value: { stringValue: 'http method' },
+                      },
+                      {
+                        key: 'http.request_content_length_uncompressed',
+                        value: { intValue: 155 },
+                      },
+                    ],
+                    spanId: 5,
+                    name: 'method name',
+                    startTimeUnixNano: 15,
+                    endTimeUnixNano: 24,
+                  },
+                ],
+              },
+            ],
+            resource: {
               attributes: [
+                { value: { stringValue: 'nextJs' } },
                 { value: 1 },
                 { value: 2 },
                 { value: 3 },
-                { key: 'http.status_code', value: { intValue: 505 } },
-                { key: 'http.flavor', value: { stringValue: 'flavor' } },
-                { key: 'http.target', value: { stringValue: 'target' } },
-                { key: 'http.method', value: { stringValue: 'http method' } },
-                {
-                  key: 'http.request_content_length_uncompressed',
-                  value: { intValue: 155 },
-                },
+                { value: { stringValue: 'internet' } },
+                { value: 5 },
+                { value: 6 },
+                { value: 7 },
+                { value: 8 },
+                { value: 9 },
+                { value: 10 },
+                { value: 11 },
+                { value: 12 },
               ],
-              spanId: 5,
-              name: 'method name',
-              startTimeUnixNano: 15,
-              endTimeUnixNano: 24,
             },
-          ],
-        },
-      ],
-      resource: {
-        attributes: [
-          { value: { stringValue: 'nextJs' } },
-          { value: 1 },
-          { value: 2 },
-          { value: 3 },
-          { value: { stringValue: 'internet' } },
-          { value: 5 },
-          { value: 6 },
-          { value: 7 },
-          { value: 8 },
-          { value: 9 },
-          { value: 10 },
-          { value: 11 },
-          { value: 12 },
+          },
         ],
-      },
-    },
-  ],
-}
-
-// //Testing for POST request to /otel route
-describe('/otel', () => {
-  it('POST', async () => {
-    const response = await request(app).post('/stream/otel').send(requestMock)
-    expect(response.body).toEqual('completed')
-    expect(response.status).toBe(200)
+      }
+      const response = await request(server)
+        .post('/stream/otel')
+        .send(requestMock)
+      expect(response.body).toEqual('completed')
+      expect(response.status).toBe(200)
+    })
   })
 })

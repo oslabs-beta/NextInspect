a21e8a20210e243ff53019f614190755be633c45
li-lucia <106473996+li-lucia@users.noreply.github.com>
2023-08-10 16:43:37 -0400

    Fixed table and reload page  responsiveness

--- chrome-extension/src/pages/app/components/Reload.tsx
+++ chrome-extension/src/pages/app/components/Reload.tsx
@@ -1,10 +1,14 @@
 const Reload = () => {
   return (
-    <div className="h-[67vh] p-56 flex flex-col justify-center text-center text-lg gap-5">
+    <div className="h-[67vh] p-10 flex flex-col items-center justify-center text-center text-sm text-gray-300 gap-5">
       <p> Recording network activity ... </p>
       <p> Perform a request or refresh the page </p>
     </div>
+
+  
   )
 }
 
 export default Reload
+
+// 
